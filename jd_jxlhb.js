89403d5ed0b9b4e382119db6710e40b5893dc407
Annyoo2021 <61466497+Annyoo2021@users.noreply.github.com>
2021-07-28 10:35:41 +0800

    Update jd_jxlhb.js

--- jd_jxlhb.js
+++ jd_jxlhb.js
@@ -1,7 +1,7 @@
 /*
 京喜领88元红包
 活动入口：京喜app-》我的-》京喜领88元红包
-助力逻辑：先自己京东账号相互助力，如有剩余助力机会，则助力作者
+助力逻辑：自己账号内部互助
 温馨提示：如提示助力火爆，可尝试寻找京东客服
 脚本兼容: Quantumult X, Surge, Loon, JSBox, Node.js
 ==============Quantumult X==============
@@ -43,7 +43,7 @@ const BASE_URL = 'https://wq.jd.com/cubeactive/steprewardv3'
   }
   console.log('京喜领88元红包\n' +
       '活动入口：京喜app-》我的-》京喜领88元红包\n' +
-      '助力逻辑：先自己京东账号相互助力，如有剩余助力机会，则助力作者\n' +
+      '助力逻辑：自己账号内部互助\n' +
       '温馨提示：如提示助力火爆，可尝试寻找京东客服')
   let res = []
   res = await getAuthorShareCode('')
